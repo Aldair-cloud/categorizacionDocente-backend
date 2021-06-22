0c5b73ec027b8f14a47287969e4e0238da74e732
Aldair-cloud <leonelsanchez@upeu.edu.pe>
2021-06-21 22:06:44 -0500

    prueba-p

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  host: "localhost",
+  host: "",
   user: "postgres",
   password: "",
   database: "",
--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-import app from "./app";
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const mimeTypes = require("mime-types");
+const cors = require("cors");
 
-app.listen(process.env.PORT || 3000);
+app.use(cors());
 
-console.log("server listen on port", 3000);
+const storage = multer.diskStorage({
+  destination: "src/uploads/",
+  filename: function (req, file, cb) {
+    cb("", file.originalname + "." + mimeTypes.extension(file.mimetype));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/view/index.html");
+});
+
+app.post("/files", upload.single("archivo"), (req, res) => {
+  res.send("Todo bien");
+});
+
+app.listen(8080, () => console.log("server start"));

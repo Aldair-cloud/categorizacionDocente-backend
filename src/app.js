34949ed82e0699df90465345201d8271a2594ff4
Aldair-cloud <leonelsanchez@upeu.edu.pe>
2021-06-27 13:42:20 -0500

    pruebaspago

--- src/app.js
+++ src/app.js
@@ -1,22 +1,51 @@
 import express from "express";
 import morgan from "morgan";
-import allRoutes from './routes/allroutes'
-
-
+import allRoutes from "./routes/allroutes";
 
 const app = express();
-var cors = require('cors');
+var cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 //RUTAS
-app.use('/CAD', allRoutes);
+app.use("/CAD", allRoutes);
 
+//SDK de Mercado de Pago
+const mercadopago = require("mercadopago");
+
+// Agrega credenciales
+mercadopago.configure({
+  access_token:
+    "TEST-4599473864722744-062705-f87dac10aaf598c16314431524bd9226-781932593",
+});
+
+app.get("/pagos", (req, res) => {
+  // Crea un objeto de preferencia
+  let preference = {
+    items: [
+      {
+        title: "Mi producto",
+        unit_price: 100,
+        quantity: 1,
+      },
+    ],
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+      global.id = response.body.id;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
 
 //DATABASE
 
-const {pg} = require('./database');
+const { pg } = require("./database");
 
 export default app;

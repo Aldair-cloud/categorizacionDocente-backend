6a228031cb2e6c3b3d2e9acb1cdb9e4be4a50b9b
mariollori <mariollori@upeu.edu.pe>
2021-06-28 17:19:52 -0500

    caso2 terminado

--- src/Controllers/persona.controllers.js
+++ src/Controllers/persona.controllers.js
@@ -31,3 +31,112 @@ export const obtenerpersonas= async(req,res)=>{
         return res.status(500).json('Error Interno....!');
     }
 }
+
+export const enviarcorreo=async(req,res)=>{
+    const {mensaje} = req.body;
+    var mailOptions = {
+        from: 'mariollori@upeu.edu.pe',
+        to: 'mariollori@upeu.edu.pe',
+        subject: 'Credenciales para acceder al sistema.',
+        text: mensaje
+      };
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'mariollori@upeu.edu.pe',
+          pass: 'dfuqnyqfqxefkyqn'
+        }
+      });
+    try {
+        transporter.sendMail(mailOptions);
+        return res.status(200).json("Exito al enviar el correo");
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json('Error Interno....!');
+    }
+  
+ 
+ 
+
+
+
+  }
+
+  export const obtenerusers = async(req,res)=>{
+
+    try {
+        const response = await pool.query('select user from usuario ');
+        return res.status(200).json(response.rows);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json('Error Interno....!');
+    }
+ 
+ 
+ 
+}
+
+export const obtenercat = async(req,res)=>{
+
+    try {
+        const response = await pool.query('select idcategoria,nombre from categoria ');
+
+        return res.status(200).json(response.rows);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json('Error Interno....!');
+    }
+ 
+ 
+ 
+}
+
+export const crearuser=async(req,res)=>{
+    const {usuario,docente} = req.body;
+
+    try {
+        const resdoc = await  pool.query('insert into docente(idcategoria,idpersona,estadocontrato,estado,estadocat) values($1,$2,$3,$4,$5)   RETURNING iddocente',[docente.idcategoria,docente.idpersona,docente.estadocontrato,true,docente.estadocat])
+        console.log(resdoc.rows[0].iddocente)
+        if(resdoc.rows[0].length!=0){
+          const resuser =  pool.query('insert into usuario(usuario,password,iddocente) values($1,$2,$3)',[usuario.user,usuario.password,resdoc.rows[0].iddocente]);
+          
+          const mensaje = `Felicidades, su solicitud ha sido aceptada.
+          Estas son las credenciales asignadas para que pueda acceder al sistema.
+          Usuario: ${usuario.user} 
+          Contraseña: ${usuario.password}`
+          enviarmensaje(mensaje)
+          
+        }
+        
+        return res.status(200).json(`Usuario ${usuario.user} registrado correctamente.`);
+        
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json('Error Interno....!');
+    }
+
+}
+function enviarmensaje(mensaje) {
+    var result;
+    var mailOptions = {
+        from: 'mariollori@upeu.edu.pe',
+        to: 'mariollori@upeu.edu.pe',
+        subject: 'Credenciales para acceder al sistema.',
+        text: mensaje
+      };
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'mariollori@upeu.edu.pe',
+          pass: 'dfuqnyqfqxefkyqn'
+        }
+      });
+    try {
+        transporter.sendMail(mailOptions);
+        result= "Exito al enviar el correo";
+    } catch (e) {
+        console.log(e);
+         result = 'Error Interno....!';
+    }
+    return result;
+  }
--- src/routes/persona.js
+++ src/routes/persona.js
@@ -6,6 +6,11 @@ router.get('/getpais', persona.listarpais);
 router.get('/getpersonas', persona.obtenerpersonas);
 
 router.post('/postpersona', persona.crearpersona);
+router.post('/postuser',persona.crearuser);
+router.get('/allpersona/:estado',persona.obtenerpersonas);
+router.get('/enviarcorreo',persona.enviarcorreo);
+router.get('/getusers',persona.obtenerusers);
+router.get('/getcat',persona.obtenercat)
 
 
 

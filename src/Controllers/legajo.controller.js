9b974bf286130be4e131c46089758097fd26e9d4
mariollori <mariollori@upeu.edu.pe>
2021-07-11 02:02:06 -0500

    terminando legajo 4

--- src/Controllers/legajo.controller.js
+++ src/Controllers/legajo.controller.js
@@ -17,7 +17,7 @@ export const crearaportes= async(req,res)=>{
 export const getaportes = async(req,res)=>{
     const iddocente = req.params.id
       try {
-          const response = await pool.query('select * from aportes where iddocente = $1',[iddocente]);
+          const response = await pool.query('select a.institucion,a.foto,a.pais,a.detalle,a.fecha,s.nombre from aportes a, subfactor s where a.idsubfactor=s.idsubfactor and  a.iddocente = $1',[iddocente]);
           return res.status(200).json(response.rows);
       } catch (e) {
          console.log(e)
@@ -52,7 +52,7 @@ export const crearpremios= async(req,res)=>{
     export const getpremios = async(req,res)=>{
         const iddocente = req.params.id
           try {
-              const response = await pool.query('select * from premios where iddocente = $1',[iddocente]);
+              const response = await pool.query('select p.institucion,p.foto,p.pais,p.detalle,p.fecha,s.nombre from premios p, subfactor s where p.idsubfactor=s.idsubfactor and  p.iddocente = $1',[iddocente]);
               return res.status(200).json(response.rows);
           } catch (e) {
              console.log(e)
@@ -69,7 +69,7 @@ export const crearasesorias= async(req,res)=>{
     const {asesoria} =req.body
    
    try {
-    const respuesta = await pool.query('insert into asesorias(ano,nivel,titulo,autor,especialidad,resolucion,foto,iddocente,idsubfactor) values($1,$2,$3,$4,$5,$6,$7,$8,$9)',[asesoria.año,asesoria.nivel,asesoria.titulo,asesoria.autor,asesoria.especialidad,asesoria.resolucion,asesoria.foto,asesoria.iddocente,asesoria.idsubfactor]) 
+    const respuesta = await pool.query('insert into asesorias(ano,nivel,titulo,autor,especialidad,resolucion,foto,iddocente,idsubfactor) values($1,$2,$3,$4,$5,$6,$7,$8,$9)',[asesoria.ano,asesoria.nivel,asesoria.titulo,asesoria.autor,asesoria.especialidad,asesoria.resolucion,asesoria.foto,asesoria.iddocente,asesoria.idsubfactor]) 
     
     
      return res.status(200).json('Asesoria/Dictamen insertado correctamente');
@@ -83,7 +83,7 @@ export const crearasesorias= async(req,res)=>{
 export const getasesorias = async(req,res)=>{
     const iddocente = req.params.id
       try {
-          const response = await pool.query('select * from asesorias where iddocente = $1',[iddocente]);
+          const response = await pool.query('select a.ano,a.titulo,a.autor,a.especialidad,a.resolucion,a.foto,s.nombre,a.nivel from asesorias a,subfactor s where a.idsubfactor=s.idsubfactor and  a.iddocente = $1',[iddocente]);
           return res.status(200).json(response.rows);
       } catch (e) {
          console.log(e)
@@ -113,7 +113,8 @@ export const crearlibros= async(req,res)=>{
 export const getlibros= async(req,res)=>{
     const iddocente = req.params.id
       try {
-          const response = await pool.query('select * from libros where iddocente = $1',[iddocente]);
+        
+          const response = await pool.query('select l.ano,l.titulo,l.rol,l.editorial,l.foto,s.nombre from libros l, subfactor s where l.idsubfactor=s.idsubfactor and  l.iddocente = $1',[iddocente]);
           return res.status(200).json(response.rows);
       } catch (e) {
          console.log(e)
@@ -143,7 +144,8 @@ export const crearproyeccion= async(req,res)=>{
 export const getproyeccion= async(req,res)=>{
     const iddocente = req.params.id
       try {
-          const response = await pool.query('select * from proyeccion_social where iddocente = $1',[iddocente]);
+       
+          const response = await pool.query('select p.nombreeven,p.fecha,p.foto,s.nombre from proyeccion_social p, subfactor s where p.idsubfactor=s.idsubfactor and  p.iddocente = $1',[iddocente]);
           return res.status(200).json(response.rows);
       } catch (e) {
          console.log(e)
@@ -178,7 +180,7 @@ export const crearcapacitacion= async(req,res)=>{
 export const getcapacitacion= async(req,res)=>{
     const iddocente = req.params.id
       try {
-          const response = await pool.query('select * from capacitacion_prof where iddocente = $1',[iddocente]);
+          const response = await pool.query('select c.titulo,c.organizadora,c.horas,c.creditos,c.lugar,c.foto,c.f_inicio,c.f_fin, s.nombre from  capacitacion_prof c, subfactor s where c.idsubfactor=s.idsubfactor and  c.iddocente = $1',[iddocente]);
           return res.status(200).json(response.rows);
       } catch (e) {
          console.log(e)
@@ -211,7 +213,7 @@ export const getparticipacion_inves= async(req,res)=>{
   const iddocente = req.params.id
     try {
       
-        const response = await pool.query('select * from Participacion_Investigacion where iddocente = $1',[iddocente]);
+        const response = await pool.query('select s.nombre,p.ano,p.titulo,p.linea_invest,p.rol,p.f_inicio,p.foto from Participacion_Investigacion p,subfactor s where p.idsubfactor=s.idsubfactor and  p.iddocente = $1',[iddocente]);
         return res.status(200).json(response.rows);
     } catch (e) {
        console.log(e)
@@ -245,7 +247,7 @@ export const crearpublic_inves= async(req,res)=>{
 export const getpublic_inves= async(req,res)=>{
   const iddocente = req.params.id
     try {
-        const response = await pool.query('select * from publicaciones_investigacion where iddocente = $1',[iddocente]);
+        const response = await pool.query('select s.nombre,p.ano,p.titulo,p.pais,p.url,p.paginas,p.foto from publicaciones_investigacion p,subfactor s where p.idsubfactor=s.idsubfactor and  p.iddocente = $1',[iddocente]);
         return res.status(200).json(response.rows);
     } catch (e) {
        console.log(e)
